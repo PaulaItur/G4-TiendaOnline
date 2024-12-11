af9b0511312b4ad7e646777d4eba89cdb49a86b7
jongonij <147401487+jongonij@users.noreply.github.com>
2024-12-11 13:50:11 +0100

    #12 #3 añado el resto de las zapatillas junto a sus fotos

--- script.js
+++ script.js
@@ -26,9 +26,16 @@ document.addEventListener("DOMContentLoaded", () => {
     // Crear las zapatillas
     const AirForce1 = new Zapatilla("AirForce1", 110, "airforce1.jpg");
     const AirMax = new Zapatilla("AirMax", 130, "airmax.jpg");
+    const DunkLow = new Zapatilla("Dunk Low", 120, "dunklow.jpg");
+    const Jordan1 = new Zapatilla("Air Jordan 1", 150, "jordan1.jpg");
+    const Pegasus40 = new Zapatilla("Pegasus 40", 130, "pegasus40.jpg");
+    const BlazerMid = new Zapatilla("Blazer Mid", 110, "blazermid.jpg");
+    const Vapormax = new Zapatilla("Air Vapormax", 200, "vapormax.jpg");
+    const ZoomFly5 = new Zapatilla("Zoom Fly 5", 160, "zoomfly5.jpg");
 
     // Lista de zapatillas
-    const zapatillas = [AirForce1, AirMax];
+    const zapatillas = [AirForce1, AirMax, DunkLow, Jordan1, Pegasus40, BlazerMid, Vapormax, ZoomFly5];
+
 
     cargarTablaZapatillas();
 });
